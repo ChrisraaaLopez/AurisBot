df2d5905e09a09f8b71368a368abaaf14921cd64
ChrisraaaLopez <chrisraaalopez14@gmail.com>
2026-04-02 01:37:40 -0600

    feat: Configuracion del bot y Contenedor de dependencias

--- AurisBot/Program.cs
+++ AurisBot/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Discord;
+using Discord.WebSocket;
+using Discord.Interactions;
+using Microsoft.Extensions.DependencyInjection;
+using DotNetEnv;
+using AurisBot.Services;
+
+// Cargar variables de entorno desde el archivo .env
+Env.Load("../.env");
+
+// Configuración del bot
+var config = new DiscordSocketConfig
+{
+    GatewayIntents = GatewayIntents.Guilds            // Eventos relacionados con servidores 
+                    | GatewayIntents.GuildMessages    // Mensajes en los canales
+                    | GatewayIntents.GuildVoiceStates, // Cambios entre canales de voz
+    
+    LogLevel = LogSeverity.Info, // Nivel de log
+};
+
+// Contenedor de dependencias
+var services = new ServiceCollection()
+    .AddSingleton(new DiscordSocketClient(config))  // Instancia cliente de discord
+    .AddSingleton<InteractionService>()             // Servicio de interacciones (comandos slash)
+    .AddSingleton<GuildGuardService>()              // Servicio para verificar que los comandos se ejecuten solo en servidores específicos
+    .AddSingleton<AudioService>()                   // Cola de canciones y reproducción de audio 
+    .BuildServiceProvider();
+
+// Variables para obtener las instancias de los servicios
+var client = services.GetRequiredService<DiscordSocketClient>();
+var interactionService = services.GetRequiredService<InteractionService>();
+var guard = services.GetRequiredService<GuildGuardService>();
+var audio = services.GetRequiredService<AudioService>();
